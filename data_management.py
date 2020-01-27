e923a570c88bd08a26ddffa68ae91aedc47b7f61
Zaratusztra <nietzscheanin@gmail.com>
2020-01-27 04:13:19 +0100

    Data management's log should now have path set to directory where script is used (even on unix! I do not intend to pust this toy into usr/bin :D).

--- data_management.py
+++ data_management.py
@@ -1,11 +1,30 @@
+import os
 import requests
 import json
 
 import logging
 
+def setup_logger(sep):
+    fname = os.path.abspath(__file__)
+    fname = fname.split(sep)
+    fname[-1] = 'data_management.log'
+    fname = sep.join(fname)
+    
+    logging.basicConfig(filename=fname, level=logging.DEBUG)
+
 def get_latest_exchange_rates(base):
-    """Placeholder"""
-    logging.basicConfig(filename='data_management.log', level=logging.DEBUG)
+    """Auxiliary function, for connecting to the api.exchangeratesapi.io API and retrieving data."""
+    if os.name == 'posix':
+        sep = '/'
+        setup_logger(sep)
+    elif os.name == 'nt':
+        sep = '\\'
+        setup_logger(sep)
+    else:
+        logging.basicConfig(filename='foreign_exchange_rates.log', level=logging.CRITICAL)
+    
+    
+    
     req = "https://api.exchangeratesapi.io/latest?base={}".format(base)
     response = requests.get(req)
     response = json.loads(response.text)
